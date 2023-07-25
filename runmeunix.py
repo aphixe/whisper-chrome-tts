6848c476532483d07d58dc12902da6d179a2e136
aphixe <aphixe@gmail.com>
2023-07-24 19:56:11 -0700

    added scripts for mac and linux
    these codes should work on mac and linux. i have tested on these systems.

---
+++ runmeunix.py
@@ -0,0 +1,30 @@
+import os
+import asyncio
+import websockets
+import subprocess
+
+async def receive_url(websocket, path):
+    while True:
+        url = await websocket.recv()
+        print("Received URL:", url)
+        
+  
+
+        # Run yt-dlp command with the URL
+        yt_dlp_command = ["yt-dlp","-o output", "-x", "--audio-format", "wav", url]
+        subprocess.run(yt_dlp_command)
+
+        batch_file_path = "mv *.wav output.wav"  # rename to output.wav
+        subprocess.run(batch_file_path, shell=True)
+
+        run_whisper_srt =["whisper","--output_format","srt", "output.wav"]
+        subprocess.run(run_whisper_srt)
+
+        batch_wav = "rm *.wav"  # Clean up all wave
+        subprocess.run(batch_wav, shell=True)
+
+
+start_server = websockets.serve(receive_url, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
